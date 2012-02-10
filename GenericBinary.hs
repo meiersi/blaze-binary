4002157d0f1552b7206a5983be549589ad0f541c
Simon Meier <iridcode@gmail.com>
2012-02-10 08:39:06 +0100

    Experiment on determining referenced types

--- GenericBinary.hs
+++ GenericBinary.hs
@@ -3,11 +3,12 @@
 {-# LANGUAGE DefaultSignatures, UndecidableInstances #-}
 
 import Data.Maybe
+import qualified Data.Set as S
 import GHC.Generics
 
 data Expr a =
          Var a
-       | Int (Maybe Int)
+       | Lit (Maybe Int)
        | Plus (Expr a) (Expr a)
        deriving( Eq, Ord, Generic )
  
@@ -75,4 +76,69 @@ instance Ser a => Ser (Expr a) where
 
 main :: IO ()
 main = putStrLn $ ser (Plus (Var 'a') (Var 'b'))
+
+data Tree b a = Leaf b a
+              | Node (Tree (b, b) a) (Tree b (a, a))
+            deriving (Generic)
+
+----
+-- Goal: gather all type names referenced by a type
+
+class Types a where
+   types :: a -> S.Set String -> S.Set String
+
+   default types :: (Generic a, GTypes (Rep a)) 
+                 => a -> S.Set String -> S.Set String
+   types x = gtypes (from x)
+
+instance Types Int where
+   types _ = S.insert "Int"
+
+instance Types Char where
+   types _ = S.insert "Char"
+
+class GTypes f where
+   gtypes :: f p -> S.Set String -> S.Set String
+
+instance (GTypes f, Datatype c) => GTypes (D1 c f) where
+   gtypes x tys
+     -- | ty `S.member` tys = tys
+     | otherwise         = gtypes (unM1 x) (S.insert ty tys)
+     where
+       ty = datatypeName x
+
+instance GTypes f => GTypes (C1 c f) where
+   gtypes _ = gtypes (undefined :: f a)
+
+instance GTypes f => GTypes (S1 c f) where
+   gtypes _ = gtypes (undefined :: f a)
+
+instance Types a => GTypes (Rec0 a) where
+   gtypes _ = id -- types (undefined :: a)
+
+instance (GTypes f, GTypes g) => GTypes (f :+: g) where
+    gtypes _ tys = gtypes (undefined :: f a) (gtypes (undefined :: g a) tys)
+
+instance (GTypes f, GTypes g) => GTypes (f :*: g) where
+    gtypes _ tys = gtypes (undefined :: f a) (gtypes (undefined :: g a) tys)
+
+instance Types a => GTypes (Par0 a) where
+    gtypes _ = types (undefined :: a)
+
+instance GTypes V1 where
+    gtypes _ tys = tys
+
+instance GTypes U1 where
+    gtypes _ tys = tys
+
+
+
+instance Types a => Types (Maybe a) where
+instance (Types a, Types b) => Types (Either a b) where
+instance Types a => Types [a] where
+
+instance Types a => Types (Expr a) where
+
+getTypes :: Types a => a -> S.Set String
+getTypes x = types x S.empty
 
