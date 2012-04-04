68e87f7aa954cdfbe72202b6d14df47e315cebdf
Simon Meier <iridcode@gmail.com>
2012-04-04 21:10:52 +0200

    some further dabbling with a Generic Binary instance

--- GenericBinary.hs
+++ GenericBinary.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleInstances, FlexibleContexts #-}
 {-# LANGUAGE DefaultSignatures, UndecidableInstances #-}
 
+import           Control.Applicative
 import           Data.Maybe
 import           Data.Monoid
 import           Blaze.ByteString.Builder
@@ -12,25 +13,32 @@ import           Numeric (showHex)
 
 import           GHC.Generics
 
+------------------------------------------------------------------------------
+-- Counting the number of constructors of a datatype
+------------------------------------------------------------------------------
 
-data Expr a =
-         Var a
-       | Lit (Maybe Int)
-       | Plus (Expr a) (Expr a)
-       deriving( Eq, Ord, Generic )
- 
-class NConstrs f where
-  gnconstrs :: f x -> Int
+class NumConstrs f where
+    gNumConstrs :: f x -> Int
 
-instance NConstrs f => NConstrs (M1 D c f) where
-  gnconstrs _ = gnconstrs (undefined :: f x) 
+instance NumConstrs f => NumConstrs (M1 D c f) where
+    gNumConstrs _ = gNumConstrs (undefined :: f x) 
 
-instance NConstrs (M1 C c f) where
-  gnconstrs _ = 1
+instance NumConstrs (M1 C c f) where
+    gNumConstrs _ = 1
 
-instance (NConstrs f, NConstrs g) => NConstrs (f :+: g) where
-  gnconstrs _ = 
-      gnconstrs (undefined :: f x) + gnconstrs (undefined :: g x)
+instance (NumConstrs f, NumConstrs g) => NumConstrs (f :+: g) where
+    gNumConstrs _ = 
+        gNumConstrs (undefined :: f x) + gNumConstrs (undefined :: g x)
+
+-- | Generic, compile time computation of the number of constructors of a
+-- datatype.
+numConstrs :: forall a. (Generic a, NumConstrs (Rep a)) => a -> Int
+numConstrs _ = gNumConstrs (from (undefined :: a))
+
+
+------------------------------------------------------------------------------
+-- Generic Serialization
+------------------------------------------------------------------------------
 
 putIntHost :: Int -> Builder
 putIntHost = fromInthost
@@ -41,72 +49,166 @@ putWord8Tag = fromInt8 . fromIntegral
 putWord16Tag :: Int -> Builder
 putWord16Tag = fromInt16be . fromIntegral
 
-nconstrs :: forall a. (Generic a, NConstrs (Rep a)) => a -> Int
-nconstrs _ = gnconstrs (from (undefined :: a))
+fromTag :: Int -> Int -> Builder
+fromTag n tag
+  | n < 0xff   = putWord8Tag tag
+  | n < 0xffff = putWord16Tag tag
+  | otherwise  = error "fromTag: more than 0xfffff constructors"
 
-class GSer f where
-  gser :: Int -> Maybe Int -> f () -> Builder
+-- The class duo responsible for serialization
+----------------------------------------------
 
-instance GSer U1 where
-  gser _ _ _ = mempty
+type Parser a = Maybe a
 
-instance Ser c => GSer (K1 i c) where
-  gser _ _ (K1 x) = ser x
+class GBinary f where
+    gToBinary
+        :: Int
+        -- ^ Maximum number of constructors of the value being serialized
+        -> Maybe Int  
+        -- ^ Number of constructers to the left of the current parameter
+        -- of the value being serialized
+        -> f a 
+        -- ^ The value to serialize
+        -> Builder
+        -- ^ Resulting sequence of bytes represented as a 'Builder'
 
-instance GSer f => GSer (M1 S c f) where
-  gser n _ (M1 x) = gser n Nothing x
+    gFromBinary
+        :: Maybe Int
+        -- ^ Number of constructers to the left of the current parameter
+        -- of the value being serialized
+        -> Parser (f a)
+        -- ^ Backup parser in case this one does not apply
+        -> (Int -> Parser (f a))
+        -- ^ Parser for final value
 
-instance GSer f => GSer (M1 D c f) where
-  gser n _ (M1 x) = gser n Nothing x
+class Binary a where
+    -- ^ Encode a value
+    toBinary   :: a -> Builder
+    fromBinary :: Parser a
 
-instance GSer f => GSer (M1 C c f) where
-  gser n Nothing    (M1 x) = gser n Nothing x
-  gser n (Just tag) (M1 x)
-    | n <= 0xff   = putWord8Tag  tag `mappend` gser n Nothing x
-    | n <= 0xffff = putWord16Tag tag `mappend` gser n Nothing x
-    | otherwise   = error "GSer: max. 0xffff constructors supported."
+    -- Generic default instance
+    default toBinary :: (Generic a, NumConstrs (Rep a), GBinary (Rep a)) 
+                     => a -> Builder
+    toBinary x = gToBinary (numConstrs x) Nothing (from x)
 
-instance (NConstrs f, GSer f, GSer g) => GSer (f :+: g) where
-  gser n off x = case x of
-      L1 l -> gser n (Just o) l
-      R1 r -> gser n (Just (o + (gnconstrs (undefined :: f ())))) r
+    default fromBinary 
+        :: (Generic a, NumConstrs (Rep a), GBinary (Rep a)) 
+        => Parser a
+    fromBinary = case numConstrs (undefined :: a) of 
+      0 -> return (error "fromBinary: phantom type - impossible")
+      1 -> to <$> gFromBinary Nothing undefined undefined 
+      _ -> error "blah"
+      
+     
+
+-- Instances for generic serialization
+--------------------------------------
+
+infixr 6 <>
+
+(<>) :: Monoid m => m -> m -> m
+(<>) = mappend
+
+instance Binary (V1 a) where
+  toBinary   = mempty
+  fromBinary = return (error "fromBinary V1: impossible")
+
+instance Binary (U1 a) where
+  toBinary    = mempty
+  fromBinary = return U1
+
+instance Binary a => Binary (K1 i f a) where
+  toBinary   (K1 x) = toBinary x
+  fromBinary        = K1 <$> fromBinary
+
+instance Binary (f a) => Binary (M1 S c f a) where
+  toBinary (M1 x) = toBinary x
+
+instance Binary (f a) => Binary (M1 D c f a) where
+  toBinary (M1 x) = toBinary x
+
+{-
+instance GBinary V1 where
+  gToBinary _ _ _   = mempty
+  gFromBinary _ _ _ = return (error "gFromBinary V1: impossible")
+
+instance GBinary U1 where
+  gToBinary _ _ _   = mempty
+  gFromBinary _ _ _ = return U1
+
+instance Binary a => GBinary (K1 i a) where
+  gToBinary _ _ (K1 x) = toBinary x
+  gFromBinary _ _ _    = K1 <$> fromBinary
+-}
+
+instance GBinary f => GBinary (M1 S c f) where
+  gToBinary n _ (M1 x) = gToBinary n Nothing x
+
+instance GBinary f => GBinary (M1 D c f) where
+  gToBinary n _ (M1 x) = gToBinary n Nothing x
+
+instance Binary (f a) => GBinary (M1 C c f) where
+  gToBinary n Nothing    (M1 x) = toBinary x
+  gToBinary n (Just tag) (M1 x) = fromTag n tag <> toBinary x
+  -- gToBinary n Nothing    (M1 x) =                  toBinary n Nothing x
+  -- gToBinary n (Just tag) (M1 x) = fromTag n tag <> gToBinary n Nothing x
+
+  -- gFromBinary Nothing backup tag = M1 <$> gFromBinary Nothing backup tag
+  -- gFromBinary (Just tag') backup tag 
+    -- | tag' == tag = M1 <$> gFromBinary
+
+instance (NumConstrs f, GBinary f, GBinary g) => GBinary (f :+: g) where
+  gToBinary n off x = case x of
+      L1 l -> gToBinary n (Just o) l
+      R1 r -> gToBinary n (Just (o + (gNumConstrs (undefined :: f a)))) r
     where
       o = fromMaybe 0 off
 
-instance (GSer f, GSer g) => GSer (f :*: g) where
-  gser n _ (x :*: y) = gser n Nothing x `mappend` gser n Nothing y
+instance (GBinary f, GBinary g) => GBinary (f :*: g) where
+  gToBinary n _ (x :*: y) = gToBinary n Nothing x <> gToBinary n Nothing y
 
-class Ser a where
-    ser :: a -> Builder
 
-    default ser :: (Generic a, NConstrs (Rep a), GSer (Rep a)) => a -> Builder
-    ser x = gser (nconstrs x) Nothing (from x)
+instance Binary Int where
+    toBinary = putIntHost
 
-instance Ser Int where
-    ser = putIntHost
+instance Binary Char where
+    toBinary = Utf8.fromChar
 
-instance Ser Char where
-    ser = Utf8.fromChar
+instance (Binary a, Binary b) => Binary (a, b) where
+instance Binary a => Binary (Maybe a) where
+instance (Binary a, Binary b) => Binary (Either a b) where
+instance Binary a => Binary [a] where
+instance Binary a => Binary (Expr a) where
 
-instance (Ser a, Ser b) => Ser (a, b) where
-instance Ser a => Ser (Maybe a) where
-instance Ser a => Ser [a] where
-instance Ser a => Ser (Expr a) where
+------------------------------------------------------------------------------
+-- Test Cases
+------------------------------------------------------------------------------
 
-testSer :: Ser a => a -> IO ()
-testSer = putStrLn . concatMap (pad . (`showHex` "")) 
-        . L.unpack . toLazyByteString . ser 
+data Expr a =
+         Var a
+       | Lit (Maybe Int)
+       | Plus (Expr a) (Expr a)
+       deriving( Eq, Ord, Generic )
+ 
+
+testBinary :: Binary a => a -> IO ()
+testBinary = putStrLn . concatMap (pad . (`showHex` "")) 
+        . L.unpack . toLazyByteString . toBinary 
     where
       pad ""  = "00"
       pad [c] = '0' : c : []
       pad cs  = cs
 
 main :: IO ()
-main = testSer $ 'a' -- (Plus (Var 'a') (Var 'b'))
+main = testBinary $ 'a' -- (Plus (Var 'a') (Var 'b'))
 
 
 e1 :: Expr String
 e1 = Plus (Var "haskell") (Lit (Just 1))
+
+------------------------------------------------------------------------------
+-- Further Experiments
+------------------------------------------------------------------------------
 
 {-
 data Tree b a = Leaf b a
@@ -128,10 +230,10 @@ class Types a where
    types x = gtypes (from x)
 
 instance Types Int where
-   types _ = S.insert "Int"
+   types _ = S.intoBinaryt "Int"
 
 instance Types Char where
-   types _ = S.insert "Char"
+   types _ = S.intoBinaryt "Char"
 
 class GTypes f where
    gtypes :: f p -> S.Set String -> S.Set String
@@ -139,7 +241,7 @@ class GTypes f where
 instance (GTypes f, Datatype c) => GTypes (D1 c f) where
    gtypes x tys
      -- | ty `S.member` tys = tys
-     | otherwise         = gtypes (unM1 x) (S.insert ty tys)
+     | otherwise         = gtypes (unM1 x) (S.intoBinaryt ty tys)
      where
        ty = datatypeName x
 
