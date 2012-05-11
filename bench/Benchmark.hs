8479864d9d691d4fdd78443db87c9186fd081420
Simon Meier <iridcode@gmail.com>
2012-05-11 18:16:53 +0200

    benchmarked attoparsec: we gain 2.4x wrt it

--- bench/Benchmark.hs
+++ bench/Benchmark.hs
@@ -13,6 +13,7 @@ module Main (main, testNewBinary) where
 import           Prelude hiding (words)
 import           Criterion.Main
 import           Control.DeepSeq
+import           Control.Applicative
 
 import           Data.Blaze.Binary.Encoding (renderTextualUtf8, renderTagged)
 import qualified Data.Blaze.Binary.Decoding       as Blaze (Decoder, runDecoder)
@@ -35,6 +36,8 @@ import           Data.Tree
 import           Data.Word
 import qualified Data.Foldable as F (toList)
 
+import qualified Data.Attoparsec as A
+
 
 ------------------------------------------------------------------------------
 -- Benchmark
@@ -42,7 +45,7 @@ import qualified Data.Foldable as F (toList)
 
 -- | The number of repetitions to consider.
 nRepl :: Int
-nRepl = 100
+nRepl = 1000
 
 -- We use NOINLINE to ensure that GHC has no chance of optimizing too much.
 
@@ -93,6 +96,10 @@ main = Criterion.Main.defaultMain $
        , bench "iter-blaze-binary: word8s" $ nf 
            (benchIterDecoder IterBlaze.word8s) 
            (Blaze.toByteString $ word8Data nRepl)
+       , bench "binary: word8s" $ nf (Binary.decode :: L.ByteString -> [Word8]) (Binary.encode $ word8Data nRepl)
+       , bench "attoparsec-noinline: word8s" $ nf 
+           (benchAttoparsec attoBinaryWord8sNoInline)
+           (Blaze.toByteString $ word8Data nRepl)
        , bench "param-blaze-binary: string" $ nf 
            (benchParamDecoder ParamBlaze.string) 
            (Blaze.toByteString $ charData nRepl)
@@ -101,7 +108,7 @@ main = Criterion.Main.defaultMain $
            (Blaze.toByteString $ charData nRepl)
     --   , bench "blaze-binary: word8sSimple" $ nf (benchDecoder Blaze.word8sSimple) (Blaze.toByteString $ word8Data nRepl)
     --   , bench "cereal: word8s" $ nf (decodeLazy :: L.ByteString -> Either String [Word8]) (encodeLazy $ word8Data nRepl)
-       , bench "binary: word8s" $ nf (Binary.decode :: L.ByteString -> [Word8]) (Binary.encode $ word8Data nRepl)
+       , bench "binary: string" $ nf (Binary.decode :: L.ByteString -> String) (Binary.encode $ charData nRepl)
        , bench "stream-blaze-binary: word8s" $ nf 
            (StreamBlaze.benchWord8s . S.copy)
            (Blaze.toByteString $ word8Data nRepl)
@@ -114,7 +121,9 @@ main = Criterion.Main.defaultMain $
            (Blaze.toByteString $ charData nRepl)
     --   , bench "blaze-binary: word8sSimple" $ nf (benchDecoder Blaze.word8sSimple) (Blaze.toByteString $ word8Data nRepl)
     --   , bench "cereal: word8s" $ nf (decodeLazy :: L.ByteString -> Either String [Word8]) (encodeLazy $ word8Data nRepl)
-       , bench "binary: string" $ nf (Binary.decode :: L.ByteString -> String) (Binary.encode $ charData nRepl)
+       , bench "attoparsec-inlined: word8s" $ nf 
+           (benchAttoparsec attoBinaryWord8s)
+           (Blaze.toByteString $ word8Data nRepl)
        ]
         
     , bgroup "encode"
@@ -127,6 +136,11 @@ main = Criterion.Main.defaultMain $
       ]
     ]
   where
+    benchAttoparsec :: A.Parser a -> S.ByteString -> a
+    benchAttoparsec p bs = case A.eitherResult $ A.parse p bs of
+      Left msg -> error msg
+      Right x  -> x
+
     benchDecoder :: Blaze.Decoder a -> S.ByteString -> a
     benchDecoder d bs = case Blaze.runDecoder d bs of
       Left msg -> error msg
@@ -170,4 +184,30 @@ instance NFData S.ByteString where
 
 instance NFData a => NFData (Seq.Seq a) where
     rnf = rnf . F.toList
+
+------------------------------------------------------------------------------
+-- Attoparsec
+------------------------------------------------------------------------------
+
+{-# INLINE genAttoBinaryWord8s #-}
+genAttoBinaryWord8s :: (A.Parser Word8) -> A.Parser [Word8]
+genAttoBinaryWord8s w8 = do
+    go
+  where
+    go = do
+        tag <- w8
+        case tag of 
+          0 -> return []
+          1 -> (:) <$> w8 <*> go
+          _ -> fail $ "parseBinaryWord8s: unknown tag " ++ show tag
+
+attoBinaryWord8s :: A.Parser [Word8]
+attoBinaryWord8s = genAttoBinaryWord8s A.anyWord8
+
+attoBinaryWord8sNoInline :: A.Parser [Word8]
+attoBinaryWord8sNoInline = genAttoBinaryWord8s attoWord8_noinline
+
+{-# NOINLINE attoWord8_noinline #-}
+attoWord8_noinline :: A.Parser Word8
+attoWord8_noinline = A.anyWord8
 
